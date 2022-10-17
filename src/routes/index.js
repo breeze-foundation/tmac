a86faf080ba115323bf008110d0984b5065730e9
NickBird <almabroral@gmail.com>
2022-10-18 02:09:00 +0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,9 +9,10 @@ const router = express.Router();
 router.use(cookieParser());
 //router.get('/new', async (req, res) => {let priceAPI = await axios.get(`https://api.bogged.finance/v1/spot_prices?tokens=0x6421282c7f14670d738f4651311c5a1286e46484&chain=bsc&api_key=wkI2Pd5mbAWe8n3I3B6C`);let price= priceAPI.data.data['0x6421282c7f14670d738f4651311c5a1286e46484'];res.render('index', { price: price })})
 router.get('', async (req, res) => {if(req.cookies.tmacRef){refVal=req.cookies.tmacRef}else{refVal=''};
-    let priceAPI = await axios.get(`https://api.bogged.finance/v1/spot_prices?tokens=0x6421282c7f14670d738f4651311c5a1286e46484&chain=bsc&api_key=wkI2Pd5mbAWe8n3I3B6C`);
-    if(priceAPI){price= priceAPI.data.data['0x6421282c7f14670d738f4651311c5a1286e46484'];}else{price='0.10'}
+    //let priceAPI = await axios.get(`https://api.bogged.finance/v1/spot_prices?tokens=0x6421282c7f14670d738f4651311c5a1286e46484&chain=bsc&api_key=wkI2Pd5mbAWe8n3I3B6C`);
+    //if(priceAPI){price= priceAPI.data.data['0x6421282c7f14670d738f4651311c5a1286e46484'];}else{price='0.10'}
     //let price= priceAPI.data.data['0x6421282c7f14670d738f4651311c5a1286e46484'];
+    let price='0.10'
     res.render('index', { price: price, refVal:refVal })})
 //router.get('/farming', async (req, res) => {if(req.cookies.tmacRef){refVal=req.cookies.tmacRef}else{refVal=''};res.render('farming',{refVal:refVal})})
 //router.get('/staking', async (req, res) => {if(req.cookies.tmacRef){refVal=req.cookies.tmacRef}else{refVal=''};res.render('staking',{refVal:refVal})})
@@ -35,8 +36,9 @@ router.get('/referral', async (req, res) => {res.render('referral')})
 router.get('/ref/:ref', async (req, res) => { 
     console.log(req.params.ref);
     if(!req.cookies.tmacRef){ if(Validator.validate(req.params.ref, 'ETH')){ res.cookie('tmacRef', req.params.ref, { expires: new Date(Date.now() + 604800000)});refVal=req.params.ref;}}else if(req.cookies.tmacRef){refVal=req.cookies.tmacRef}else{refVal=''};
-    let priceAPI = await axios.get(`https://api.bogged.finance/v1/spot_prices?tokens=0x6421282c7f14670d738f4651311c5a1286e46484&chain=bsc&api_key=wkI2Pd5mbAWe8n3I3B6C`);
-    if(priceAPI){price= priceAPI.data.data['0x6421282c7f14670d738f4651311c5a1286e46484'];}else{price='0.10'}
+    //let priceAPI = await axios.get(`https://api.bogged.finance/v1/spot_prices?tokens=0x6421282c7f14670d738f4651311c5a1286e46484&chain=bsc&api_key=wkI2Pd5mbAWe8n3I3B6C`);
+    //if(priceAPI){price= priceAPI.data.data['0x6421282c7f14670d738f4651311c5a1286e46484'];}else{price='0.10'}
+    let price='0.10'
     res.render('index', { price: price, refVal:refVal }) })
 router.get('/robots.txt', function (req, res) { res.type('text/plain'); res.send("User-agent: *\nDisallow:"); });
 module.exports = router;
